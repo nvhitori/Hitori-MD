c2460ae0ed4fa5967ce6c1a4884441cca2f60ed8
V4LL <122162748+nvhitori@users.noreply.github.com>
2023-01-28 17:40:29 +0700

    XHITORI

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('KANNABOT\nMD', {
+say('XHITORI\nMD', {
   font: 'block',
   align: 'center',
   colors: ['blue']
