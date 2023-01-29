ac333e3832444eb0af4d60aa00d0f86026ef7269
V4LL <122162748+nvhitori@users.noreply.github.com>
2023-01-29 15:55:39 +0700

    Fix

--- plugins/vote-cekvote.js
+++ plugins/vote-cekvote.js
@@ -20,7 +20,7 @@ ${devote.map(u => '@' + u.split('@')[0]).join('\n')}}
 
 *${usedPrefix}hapusvote* - untuk menghapus vote
 
-_by ᴹᴿ᭄ King Of Bear ×፝֟͜×_
+_by Gopala5u_
 `.trim(), false, { contextInfo: { mentionedJid } })
 }
 handler.help = ['cekvote']
